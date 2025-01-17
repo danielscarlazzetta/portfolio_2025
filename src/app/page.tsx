3eb568c3be2ea19657f3d0e24636e764203e56be
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2025-01-17 01:24:41 -0300

    create nodemail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,65 @@ import { technologies } from "@/data/technologies";
 import { proyectos } from "@/data/proyects";
 
 import { FaGit, FaLinkedinIn } from "react-icons/fa6";
+import Contact from "@/component/contact";
 
 export default function Home() {
   return (
     <div>
       <header className="bg-base-200 shadow-md">
-        <div className="navbar container mx-auto">
-          <div className="flex-1">
-            <a className="text-xl font-bold">Daniel Scarlazzetta</a>
+        <div className="navbar bg-base-100">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <a>Sobre mi</a>
+                </li>
+                <li>
+                  <a>Proyectos</a>
+                </li>
+                <li>
+                  <a>Tecnologias</a>
+                </li>
+                <li>
+                  <a>Contacto</a>
+                </li>
+              </ul>
+            </div>
+            <a className="btn btn-ghost text-xl">Daniel Scarlazzetta</a>
           </div>
-          <div className="flex-none">
+          <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">
               <li>
-                <a>Sobre mí</a>
+                <a>Sobre mi</a>
               </li>
               <li>
                 <a>Proyectos</a>
               </li>
               <li>
-                <a>Tecnologías</a>
+                <a>Tecnologias</a>
               </li>
               <li>
                 <a>Contacto</a>
@@ -31,7 +71,7 @@ export default function Home() {
         </div>
       </header>
 
-      <section className="grid grid-cols-1 md:grid-cols-2 m-12 p-12  ">
+      <section className="grid grid-cols-1 md:grid-cols-2 m-12 p-12">
         <div className="self-center">
           <h1 className="text-3xl font-bold mt-4">Daniel Scarlazzetta</h1>
           <p className="text-lg text-gray-600 mt-2">
@@ -68,7 +108,8 @@ export default function Home() {
           tecnica santo tomás, e complementado mis estudios en bootcam de alura
           latam, cursos de udemy y videos en youtube.
         </p>
-        <p  className="text-lg text-gray-600 mt-2">Dentro de mi juventud e trabajado en diversos rubros durante mi vida,
+        <p className="text-lg text-gray-600 mt-2">
+          Dentro de mi juventud e trabajado en diversos rubros durante mi vida,
           ya sea como vendendor, cajero, ingeniero fullstac, hasta dechofer
         </p>
       </section>
@@ -136,6 +177,10 @@ export default function Home() {
             Enviar
           </button>
         </form>
+      </section>
+
+      <section className="container mx-auto my-10">
+        <Contact />
       </section>
 
       <footer className="bg-base-200 py-4">
